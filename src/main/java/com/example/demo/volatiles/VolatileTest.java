5946a1d1bf3318a1df1b7c11bf8844c1f25be3e8
langinteger <709091988@qq.com>
2021-08-01 23:26:15 +0800

    feat - add volatile test

---
+++ src/main/java/com/example/demo/volatiles/VolatileTest.java
@@ -0,0 +1,40 @@
+package com.example.demo.volatiles;
+
+/**
+ * https://stackoverflow.com/questions/68599358/volatile-variables-does-not-work-as-expected
+ *
+ * The actual expectation to volatile here is that with it you cannot get myI and myI1 with a diff greater than 1 in
+ * the watch thread. And without it, the result will be a mess.
+ */
+public class VolatileTest {
+  private static volatile int i = 0;
+  private static volatile int i1 = 0;
+
+  public static void main(String[] args) throws InterruptedException {
+    Thread t = new Thread(VolatileTest::writerThread);
+    Thread t1 = new Thread(VolatileTest::readerThread);
+    t.start();
+    t1.start();
+    t.join();
+    t1.join();
+  }
+
+  public static void writerThread() {
+    for (int j = 0; j < 100000; j++) {
+      i = j;
+      i1 = j;
+    }
+  }
+
+  public static void readerThread() {
+    int myI, myI1;
+    for (int j = 0; j < 100000; j++) {
+      //
+      myI = i;
+      myI1 = i1;
+      if (myI1 > myI) {
+        System.out.println(myI + " < " + myI1);
+      }
+    }
+  }
+}
